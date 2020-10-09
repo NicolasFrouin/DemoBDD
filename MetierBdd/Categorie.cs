276d9b0976b8f7dedb43321feb68a7e5a7a22d6b
Nicolas FROUIN <nfrouin@lerebours.org>
2020-10-09 16:00:26 +0200

    Ajoutez des fichiers projet.

---
+++ MetierBdd/Categorie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MetierBdd
+{
+    public class Categorie
+    {
+        public int NumCateg { get; set; }
+        public string LibelleCateg { get; set; }
+        public string PhotoCateg { get; set; }
+
+        public Categorie(int unNum, string unLibelle, string unePhoto)
+        {
+            NumCateg = unNum;
+            LibelleCateg = unLibelle;
+            PhotoCateg = unePhoto;
+        }
+    }
+}
---
+++ MetierBdd/GestionnaireBdd.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetierBdd
+{
+    public class GestionnaireBdd
+    {
+        // cnx sert à se connecter à la BDD
+        private MySqlConnection cnx;
+        // cmd sert à écrire nos requêtes SQL
+        private MySqlCommand cmd;
+        // dr me permet de récupérer le jeu d'enregistrement(s) de ma requête (SELECT)
+        private MySqlDataReader dr;
+
+        // Constructeur
+        public GestionnaireBdd()
+        {
+            // La chaine va nous permettre de donner 
+            // 1) le nom du serveur
+            // 2) le nom de la base de données
+            // 3) le nom de l'utilisateur
+            // 4) le mot de passe
+            string chaine = "Server=localhost;Database=ludotheque;Uid=root;Pwd=";
+            cnx = new MySqlConnection(chaine);
+            cnx.Open();
+        }
+    }
+}
